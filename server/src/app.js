b3dab2010c522877d85a069595e57efec5f8af04
lmislm <wszdlm@gmail.com>
2018-04-05 23:21:27 +0800

    解决vscode保存格式化问题

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,18 @@
-const express = require('express')
-const bodyParse = require('body-parser') //process JSON data easily
-const cors = require('cors')
-const morgan = require('morgan')
-const {sequelize} = require('./models')
-const config = require('./config/config')
+const express = require('express');
+const bodyParse = require('body-parser'); //process JSON data easily
+const cors = require('cors');
+const morgan = require('morgan');
+const { sequelize } = require('./models');
+const config = require('./config/config');
 
-const app = express()
-app.use(morgan('combined'))
-app.use(bodyParse.json())
-app.use(cors()) // security
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParse.json());
+app.use(cors()); // security
 
-require('./routes')(app)
+require('./routes')(app);
 
-sequelize.sync()
-    .then(() => {
-        app.listen(config.port)
-        console.log(`Server started on port ${config.port}`)   //注意符号`和'
-    })
+sequelize.sync().then(() => {
+  app.listen(config.port);
+  console.log(`Server started on port ${config.port}`); //注意符号`和'
+});
--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,14 +1,14 @@
-const {User} = require('../models')
+const { User } = require('../models');
 
 module.exports = {
-   async register(req,res) {
+  async register(req, res) {
     try {
-        const user = await User.create(req.body)
-        res.send(user.toJSON())
+      const user = await User.create(req.body);
+      res.send(user.toJSON());
     } catch (err) {
-        res.status(400).send({
-            error: '这个邮箱已经被使用了'
-        })
+      res.status(400).send({
+        error: '这个邮箱已经被使用了'
+      });
     }
-    }
-}
+  }
+};
